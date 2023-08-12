0bd5facb8a049c345ae8bf208738ab4f4c9dd9f3
Aleksandr <pulatov.alexander@gmail.com>
2023-08-12 22:16:15 +0300

    Initial Commit

---
+++ FunnyCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FunnyCalculator
+//
+//  Created by Alexander on 12.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FunnyCalculator/FunnyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyCalculatorApp.swift
+//  FunnyCalculator
+//
+//  Created by Alexander on 12.08.23.
+//
+
+import SwiftUI
+
+@main
+struct FunnyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

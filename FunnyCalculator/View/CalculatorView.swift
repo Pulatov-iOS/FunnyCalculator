ca76a953ea0881e6016b81a752a1e3c641adffa6
Aleksandr <pulatov.alexander@gmail.com>
2023-08-20 16:56:56 +0300

    The main page of the calculator has been changed

--- FunnyCalculator/View/CalculatorView.swift
+++ FunnyCalculator/View/CalculatorView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct CalculatorView: View {
+    
+    @State private var result: String = "n"
+    
     var body: some View {
         VStack {
+            Text(result)
+                .foregroundColor(.black)
+                .lineLimit(1)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!2 ")
         }
         .padding()
     }

8106a02b5f919ccf4a9922556eb0c481b7ee97ea
Aleksandr <pulatov.alexander@gmail.com>
2023-08-22 23:26:10 +0300

    Added a button in the main window

--- FunnyCalculator/View/CalculatorView.swift
+++ FunnyCalculator/View/CalculatorView.swift
@@ -26,6 +26,9 @@ struct CalculatorView: View {
             Button("+"){
                 
             }
+            Button("-"){
+                
+            }
         }
         .padding()
     }
